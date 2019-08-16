8d5829ebf50c3c7ab748fa389e928abc0c98d2bc
hany elshahawy <hany.ea.elshahawy@partner.bmw.de>
2019-08-16 19:13:04 +0200

    Add knapsack problem

--- dynamic_programming/main.cpp
+++ dynamic_programming/main.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 #include "factorial.h"
 #include "fibonacci.h"
+#include "knapsack.h"
+#include <vector>
 
 int main() {
-    int n = 7;
+    vector<int> value {60,100,120};
+    vector<int> weight {10,20,30};
+    int W = 50;
+    std::cout << "********* knapsack **********" << std::endl;
+    std::cout << knapsack_rec(value, weight, W, static_cast<int>(value.size()) - 1) << std::endl;
+    std::cout << knapsack_rec_dp(value, weight, W, static_cast<int>(value.size())) << std::endl;
+    int n = 9;
     std::cout << "********* fibonacci **********" << std::endl;
     std::cout << fib_rec(n) << std::endl;
-    std::cout << fac_db_tabulation(n) << std::endl;
-    std::cout << fac_db_memoization(n) << std::endl;
+    std::cout << fib_dp_tabulation(n) << std::endl;
+    std::cout << fib_dp_memoization(n) << std::endl;
 
     std::cout << "********* factorial **********" << std::endl;
     std::cout << fac_rec(n) << std::endl;
-    std::cout << fac_db_tabulation(n) << std::endl;
-    std::cout << fac_db_memoization(n) << std::endl;
+    std::cout << fac_dp_tabulation(n) << std::endl;
+    std::cout << fac_dp_memoization(n) << std::endl;
     return 0;
 }
