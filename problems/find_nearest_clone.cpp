995b8af6f79802ad8474d07e06baa9a404b8d83f
Hany Elshahawy <elshahawy@ds1.internal>
2019-11-13 14:21:46 +0100

    Find nearest clone graph problem

--- problems/find_nearest_clone.cpp
+++ problems/find_nearest_clone.cpp
@@ -4,8 +4,9 @@
 
 #include <iostream>
 #include <vector>
-#include <memory>
 #include <unordered_map>
+#include <set>
+#include <deque>
 
 using namespace std;
 
@@ -22,32 +23,71 @@ using namespace std;
 struct node
 {
   int color{0};
+  int path{0};
   vector<node*> neighbours{};
 };
 class di_graph
 {
 private:
-  unique_ptr<vector<node>> nodes_{nullptr};
+  vector<node> nodes_;
   unordered_map<long, int> nodes_ids{};
 public:
   di_graph(int graph_nodes, vector<int>& graph_from_, vector<int>& graph_to_,
-           vector<long>& ids_)
+           vector<long>& ids_) : nodes_(vector<node>(graph_nodes,node{}))
   {
-    nodes_ = make_unique<vector<node>>(graph_nodes,node{});
     for(auto i{0}; i < ids_.size(); ++i)
     {
       nodes_ids[ids_[i]]++;
-      (*nodes_)[i].color = ids_[i];
+      nodes_[i].color = ids_[i];
     }
     for (size_t i{0}; i < graph_from_.size(); ++i)
     {
-      auto ref{&(nodes_->at(graph_to_[i]-1))};
-      (*nodes_)[graph_from_[i]-1].neighbours.emplace_back(ref);
+      auto ref{&(nodes_.at(graph_to_[i]-1))};
+      nodes_[graph_from_[i]-1].neighbours.emplace_back(ref);
+      ref->neighbours.emplace_back(&(nodes_[graph_from_[i]-1]));
     }
+  }
+  int breadth_first_search_for_same_color(node* source)
+  {
+    int path{0};
+    std::set<node*> visited{};
+    std::deque<node*> unvisited{};
+    unvisited.emplace_back(source);
+    while (!unvisited.empty())
+    {
+      auto n{unvisited.front()};
+      visited.emplace(n);
+      unvisited.pop_front();
+      for(auto i{0}; i < n->neighbours.size(); ++i)
+      {
+        if (visited.find(n->neighbours[i]) == visited.end())
+        {
+          n->neighbours[i]->path = n->path + 1;
+          if(n->neighbours[i]->color == source->color)
+          {
+            return n->neighbours[i]->path;
+          } else
+          {
+            unvisited.emplace_back(n->neighbours[i]);
+          }
+        }
+      }
+    }
+    return path;
   }
   int get_shortest_path(int val)
   {
-        
+    int shortest_path{999};
+    for(size_t i{0}; i < nodes_.size(); ++i)
+    {
+      if(nodes_[i].color == val)
+      {
+        auto value{breadth_first_search_for_same_color(&(nodes_[i]))};
+        if (value != 0 and value < shortest_path)
+          shortest_path = value;
+      }
+    }
+    return shortest_path;
   }
   bool is_path_possible(int val)
   {
@@ -67,7 +107,7 @@ int findShortest(int graph_nodes, vector<int> graph_from, vector<int> graph_to,
   di_graph g(graph_nodes, graph_from, graph_to, ids);
   if (g.is_path_possible(val))
   {
-    ans = 5;
+    ans = g.get_shortest_path(val);
   }
   return ans;
 
@@ -80,7 +120,8 @@ int main()
   vector<int> graph_to{2,3,2};
   vector<long> ids{1,2,1,1};
   int val{1};
-  int ans = findShortest(graph_nodes, graph_from, graph_to, ids, val);
+  int ans;
+  ans = findShortest(graph_nodes, graph_from, graph_to, ids, val);
   std::cout << "expected answer: 1" << "\n";
   std::cout << "answer is " << ans << "\n";
 
